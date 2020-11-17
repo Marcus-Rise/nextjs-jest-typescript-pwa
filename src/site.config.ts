d055db158b9a837aa55c08321aadf83466d7f963
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-17 18:04:10 +0400

    feat(site.config.ts)

---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+const siteConfig = {
+  title: `Архитектура Frontend`,
+  date: `Ноябрь 18, 2020`,
+  author: {
+    name: `Илья Константинов`,
+    url: `https://marcus-rise.dev`,
+  },
+};
+
+export { siteConfig };
