87cd2096eafb09e58a3ddb596e3f7aaa359feefb
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-17 18:03:53 +0400

    feat: mdx

--- next.config.js
+++ next.config.js
@@ -1,23 +1,28 @@
 /* eslint-disable */
 const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV !== "production",
-    dest: "public",
-  },
-  crossOrigin: "anonymous",
-  images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    domains: ["images.prismic.io"],
-  },
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
-    });
-
-    return config;
-  },
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
 });
+
+module.exports = withMDX(
+  withPWA({
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    pwa: {
+      disable: process.env.NODE_ENV !== "production",
+      dest: "public",
+    },
+    crossOrigin: "anonymous",
+    images: {
+      deviceSizes: [320, 420, 768, 1024, 1200],
+    },
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack", "url-loader"],
+      });
+
+      return config;
+    },
+  }),
+);
