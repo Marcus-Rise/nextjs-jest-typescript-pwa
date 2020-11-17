ff05edd75f87036941c5ac6bc17d6594a6781fc3
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-17 18:04:43 +0400

    feat(_app.tsx): apply header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Head from "next/head";
 import "../src/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Header } from "../src/components/header";
+import { siteConfig } from "../src/site.config";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -9,6 +11,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <title>Title</title>
       </Head>
+      <Header {...siteConfig} />
       <main>
         <Component {...pageProps} />
       </main>
