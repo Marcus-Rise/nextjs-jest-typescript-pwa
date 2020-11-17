af369c419b5c602c6c4f3f87c7829e5e551cd030
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-17 18:40:43 +0400

    feat(components/footer)

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.scss";
+
+const Footer: React.FC<{ pagesTotalCount: number; pageCurrentNumber: number }> = (props) => {
+  return (
+    <footer className={styles.root}>
+      {props.pageCurrentNumber}/{props.pagesTotalCount}
+    </footer>
+  );
+};
+
+export { Footer };
