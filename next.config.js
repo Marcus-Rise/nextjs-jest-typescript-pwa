3aa1be0b8802ac559d5c6a513f693a2a604924cd
Ilya Konstantinov <ilykonst95@gmail.com>
2020-11-17 19:10:46 +0400

    refactor(next.config.js)

--- next.config.js
+++ next.config.js
@@ -4,25 +4,25 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
-module.exports = withMDX(
-  withPWA({
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-    pwa: {
-      disable: process.env.NODE_ENV !== "production",
-      dest: "public",
-    },
-    crossOrigin: "anonymous",
-    images: {
-      deviceSizes: [320, 420, 768, 1024, 1200],
-    },
-    reactStrictMode: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"],
-      });
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  pwa: {
+    disable: process.env.NODE_ENV !== "production",
+    dest: "public",
+  },
+  crossOrigin: "anonymous",
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack", "url-loader"],
+    });
 
-      return config;
-    },
-  }),
-);
+    return config;
+  },
+};
+
+module.exports = withMDX(withPWA(nextConfig));
